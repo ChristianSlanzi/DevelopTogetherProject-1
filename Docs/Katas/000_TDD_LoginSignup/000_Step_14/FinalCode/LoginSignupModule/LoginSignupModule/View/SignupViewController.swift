04bf23d2a4f3caed1f1bfc4d3262c59183e3e624
Christian Slanzi <christian.slanzi@gmail.com>
2021-03-24 13:18:34 +0100

    add Step 14 - Binding the login method

---
+++ Docs/Katas/000_TDD_LoginSignup/000_Step_14/FinalCode/LoginSignupModule/LoginSignupModule/View/SignupViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SignupViewController.swift
+//  LoginSignupModule
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+        emailAddressTextField.delegate = self
+    }
+
+    @IBAction func create (_ sender: Any) {
+
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func userNameDidEndOnExit(_ sender: Any) {
+        
+    }
+    
+    @IBAction func passwordDidEndOnExit(_ sender: Any) {
+
+    }
+    
+    @IBAction func confirmPasswordDidEndOnExit(_ sender: Any) {
+
+    }
+    
+    @IBAction func emailAddressDidEndOnExit(_ sender: Any) {
+
+    }
+}
+
+extension SignupViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        if let updatedString = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) {
+            print(updatedString)
+        }
+        
+        return true
+    }
+}
---
+++ Docs/Katas/000_TDD_LoginSignup/000_Step_14/FinalCode/LoginSignupModule/LoginSignupModuleTests/Mocks/MockEmailAddressValidator.swift
@@ -0,0 +1,32 @@
+//
+//  MockEmailAddressValidator.swift
+//  LoginSignupModuleTest
+//
+
+import Foundation
+import XCTest
+@testable import LoginSignupModule
+
+class MockEmailAddressValidator: EmailAddressValidator {
+    
+    private var expectation: XCTestExpectation?
+    private var expectedValue: String?
+    
+    init(_ expectation: XCTestExpectation, expectedValue: String) {
+        self.expectation = expectation
+        self.expectedValue = expectedValue
+        super.init()
+    }
+    
+    override func validate(_ value: String) -> Bool {
+        
+        if let expectation = self.expectation,
+            let expectedValue = self.expectedValue {
+            if value.compare(expectedValue) == .orderedSame {
+                expectation.fulfill()
+            }
+        }
+        
+        return super.validate(value)
+    }
+}
