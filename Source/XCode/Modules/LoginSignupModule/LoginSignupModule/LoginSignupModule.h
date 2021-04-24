aa11c9ee5ac2dda338baa2024e5b4452d938cb30
Christian Slanzi <christian.slanzi@gmail.com>
2021-04-24 19:16:24 +0200

    create LoginSignupModule project

---
+++ Source/XCode/Modules/LoginSignupModule/LoginSignupModule/LoginSignupModule.h
@@ -0,0 +1,18 @@
+//
+//  LoginSignupModule.h
+//  LoginSignupModule
+//
+//  Created by Christian Slanzi on 24.04.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginSignupModule.
+FOUNDATION_EXPORT double LoginSignupModuleVersionNumber;
+
+//! Project version string for LoginSignupModule.
+FOUNDATION_EXPORT const unsigned char LoginSignupModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginSignupModule/PublicHeader.h>
+
+
