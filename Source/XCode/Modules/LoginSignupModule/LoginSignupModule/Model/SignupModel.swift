09bcb464f988fd4cbcdb1cc101435901de038840
Christian Slanzi <christian.slanzi@gmail.com>
2021-04-25 16:54:04 +0200

    TDD EmailAddressValidator to build a SignupModel class in the same way as the LoginModel.

---
+++ Source/XCode/Modules/LoginSignupModule/LoginSignupModule/Model/SignupModel.swift
@@ -0,0 +1,42 @@
+//
+//  SignupModel.swift
+//  LoginSignupModule
+//
+//  Created by Christian Slanzi on 25.04.21.
+//
+
+import Foundation
+
+public class SignupModel {
+
+    var userName: String
+    var password: String
+    var emailAddress: String
+    
+    public init?(userName: String, password: String, emailAddress: String,
+          userNameValidator: UserNameValidator? = nil,
+          passwordValidator: PasswordValidator? = nil,
+          emailAddressValidator: EmailAddressValidator? = nil) {
+        
+        let validator1 = userNameValidator ?? UserNameValidator()
+        if validator1.validate(userName) == false {
+            return nil
+        }
+        
+        let validator2 = passwordValidator ?? PasswordValidator()
+        if validator2.validate(password) == false {
+            return nil
+        }
+        
+        let validator3 = emailAddressValidator ?? EmailAddressValidator()
+        if validator3.validate(emailAddress) == false {
+            return nil
+        }
+        
+        self.userName = userName
+        self.password = password
+        self.emailAddress = emailAddress
+        
+    }
+    
+}
