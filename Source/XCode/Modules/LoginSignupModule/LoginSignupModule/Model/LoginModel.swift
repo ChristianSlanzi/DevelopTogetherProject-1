7f7b6a9b3ab9de3844e86bc4006dcbd2fef0c6a6
Christian Slanzi <christian.slanzi@gmail.com>
2021-04-25 11:38:44 +0200

    fix to pass testLoginModel_InvalidUserName_ValidPassword_CanNotBeInstantiated

--- Source/XCode/Modules/LoginSignupModule/LoginSignupModule/Model/LoginModel.swift
+++ Source/XCode/Modules/LoginSignupModule/LoginSignupModule/Model/LoginModel.swift
@@ -12,6 +12,10 @@ public class LoginModel {
     public init?(userName: String,
           password: String,
           userNameValidator: UserNameValidator? = nil) {
-
+        
+        let validator1 = userNameValidator ?? UserNameValidator()
+        if validator1.validate(userName) == false {
+            return nil
+        }
     }
 }
