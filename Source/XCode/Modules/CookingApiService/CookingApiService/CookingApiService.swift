9ffa5a6351877d8b5ba75d5ba6a62f310b9d4d47
Christian Slanzi <christian.slanzi@gmail.com>
2021-04-26 16:31:20 +0200

    add a service factory

--- Source/XCode/Modules/CookingApiService/CookingApiService/CookingApiService.swift
+++ Source/XCode/Modules/CookingApiService/CookingApiService/CookingApiService.swift
@@ -5,11 +5,25 @@
 //  Created by Christian Slanzi on 26.04.21.
 //
 
-import Foundation
+import NetworkingService
 
 public protocol CookingApiService {
     typealias ServiceError = CookingApiServiceError
     typealias RecipesSearchResult = Swift.Result<RecipesSearchResultDTO, ServiceError>
     
     func searchRecipes(completion: @escaping (RecipesSearchResult) -> Void)
+}
+
+public class CookingApiServiceFactory {
+    private let url: URL
+    private var client: HTTPClient
+    private var apiKey: String?
+    public init(url: URL, client: HTTPClient, apiKey: String? = nil) {
+        self.url = url
+        self.client = client
+        self.apiKey = apiKey
+    }
+    public func getCookingApiService() -> CookingApiService {
+        return CookingApiRemote(url: url, client: client, apiKey: apiKey)
+    }
 }
