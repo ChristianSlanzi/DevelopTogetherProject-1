c6a1affa4b2a708f3113a23df3fd731ea742df3c
Christian Slanzi <christian.slanzi@gmail.com>
2021-04-26 10:12:48 +0200

    implemented the SignupController. Like the LoginController, the signup controller is a business object that performs a signup with a validated signup model.

---
+++ Source/XCode/Modules/LoginSignupModule/LoginSignupModule/Controllers/SignupController.swift
@@ -0,0 +1,55 @@
+//
+//  SignupController.swift
+//  LoginSignupModule
+//
+//  Created by Christian Slanzi on 26.04.21.
+//
+
+import Foundation
+
+public protocol SignupControllerDelegate: class {
+    func signupResult(result: Bool, error: String?)
+}
+
+public class SignupController {
+    
+//    let dummyUsername = "Alibaba"
+//    let dummyPassword = "Sesamo123"
+//    let dummyEmailAddress = "a@b.com"
+    
+    weak var signupControllerDelegate: SignupControllerDelegate?
+    
+    public var userApiService: LoginSignupService?
+    
+    public init(delegate: SignupControllerDelegate?) {
+        self.signupControllerDelegate = delegate
+    }
+    
+    public func doSignup(model: SignupModel) {
+        
+        //let userName = model.userName
+        let emailAddress = model.emailAddress
+        let password = model.password
+        
+        userApiService?.register(email: emailAddress, password: password, completion: { (result) in
+            
+            switch result {
+            case .success(let registerData):
+                print("registerData: " + registerData.description)
+                
+                DispatchQueue.main.async {
+                    self.signupControllerDelegate?.signupResult(result: true, error: nil)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+                DispatchQueue.main.async {
+                    self.signupControllerDelegate?.signupResult(result: false, error: error.localizedDescription)
+                }
+            }
+        })
+        
+    }
+    
+}
