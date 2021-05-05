d8aa80e0d8d43e6128ed114faa29922466152046
Christian Slanzi <christian.slanzi@gmail.com>
2021-05-05 23:31:52 +0200

    create RecipeStore framework project

---
+++ Source/XCode/Modules/RecipeStore/RecipeStore/RecipeStore.h
@@ -0,0 +1,18 @@
+//
+//  RecipeStore.h
+//  RecipeStore
+//
+//  Created by Christian Slanzi on 05.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeStore.
+FOUNDATION_EXPORT double RecipeStoreVersionNumber;
+
+//! Project version string for RecipeStore.
+FOUNDATION_EXPORT const unsigned char RecipeStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeStore/PublicHeader.h>
+
+
