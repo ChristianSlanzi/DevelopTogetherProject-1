cc7a0789d1842c6f9a4db6b9a23d832593f29307
Christian Slanzi <christian.slanzi@gmail.com>
2021-05-16 13:17:32 +0200

    add view model layer and protocols

---
+++ Source/XCode/Modules/RecipeUI/RecipeUI/ViewModel/CollectionViewModel.swift
@@ -0,0 +1,110 @@
+//
+//  CollectionViewModel.swift
+//  RecipeBook
+//
+//  Created by Christian Slanzi on 09.05.21.
+//
+
+import Foundation
+import RecipeFeature
+
+public class CollectionViewModel {
+    
+    weak var view: CollectionViewControllerProtocol?
+    public var recipeLoader: RecipeLoader?
+    var recipeBook: RecipeBook?
+    
+    
+    public init(view: CollectionViewControllerProtocol) {
+        self.view = view
+        
+        if recipeBook == nil {
+            recipeBook = RecipeBook()
+        }
+    }
+    
+    func performInitialViewSetup() {
+        
+        view?.setNavigationTitle("Recipes Book")
+        view?.setSectionInset(top: 20, left: 0, bottom: 0, right: 0)
+        view?.setupCollectionViewCellToUseMaxWidth()
+        
+        recipeLoader?.load(completion: { (result) in
+            switch result {
+            case let .success(recipes):
+                guard !recipes.isEmpty else {
+                    let path = Bundle.main.path(forResource: "RecipeBook", ofType: "plist")
+                    self.recipeBook?.load(filePath: path)
+                    return
+                }
+                let category = RecipeCategory(id: 99, title: "", recipes: recipes.map({ (recipe) -> Recipe in
+                    Recipe(id: recipe.id, title: recipe.title, image: recipe.image, imageType: recipe.imageType)
+                }))
+                self.recipeBook?.categories?.append(category)
+            case let .failure(error):
+                print(error)
+                let path = Bundle.main.path(forResource: "RecipeBook", ofType: "plist")
+                self.recipeBook?.load(filePath: path)
+            }
+            self.view?.reload()
+        })
+    }
+    
+    func numberOfSections() -> Int {
+        return recipeBook?.categories?.count ?? 0
+    }
+    
+    func numberOfItemsInSection(_ section: Int) -> Int {
+        guard let recipeBook = recipeBook,
+            let categories = recipeBook.categories else {
+                return 0
+        }
+        
+        if ((section < 0) || (section >= categories.count)) {
+            return 0
+        }
+        
+        guard let recipes = categories[section].recipes else {
+            return 0
+        }
+        
+        return recipes.count
+    }
+    
+    func cellViewModel(row: Int, section: Int) -> CollectionViewCellViewModel? {
+        
+        guard let recipeBook = recipeBook,
+              let categories = recipeBook.categories else {
+                return nil
+        }
+        
+        if ((section < 0) || (section >= categories.count)) {
+            return nil
+        }
+        
+        guard let recipes = categories[section].recipes else {
+            return nil
+        }
+        
+        if ((row < 0) || (row >= recipes.count)) {
+            return nil
+        }
+        
+        return CollectionViewCellViewModel(model: recipes[row])
+    }
+    
+    func headerViewModel(section: Int) -> CollectionViewSectionHeaderViewModel? {
+        
+        guard let recipeBook = recipeBook,
+              let categories = recipeBook.categories else {
+            return nil
+            
+        }
+        
+        if ((section < 0) || (section >= categories.count)) {
+            return nil
+        }
+        
+        return CollectionViewSectionHeaderViewModel(model: categories[section].title)
+    }
+}
